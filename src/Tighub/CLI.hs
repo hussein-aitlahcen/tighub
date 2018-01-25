c136b4976af394d44e4a12ed938bbad5812808b4
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-01-25 21:28:40 +0000

    Add: cli entrypoint

---
+++ src/Tighub/CLI.hs
@@ -0,0 +1,54 @@
+-- CLI.hs ---
+
+-- Copyright (C) 2018 Hussein Ait-Lahcen
+
+-- Author: Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
+
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 3
+-- of the License, or (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+{-# LANGUAGE DeriveDataTypeable #-}
+
+module Tighub.CLI
+  (
+    entryPoint
+  ) where
+
+import           System.Console.CmdArgs
+
+type Remote = String
+type Branch = String
+
+data Command = Pull
+             | Push
+             | Status
+             deriving (Show, Data)
+
+data Arguments = Arguments { cmd :: Command
+                           , remote :: Remote
+                           , branch :: Branch
+                           }
+               deriving (Show, Data, Typeable)
+
+defaultArguments :: Arguments
+defaultArguments = Arguments { cmd = Status
+                             , remote = "remote"
+                             , branch = "branch"
+                             }
+
+entryPoint :: IO ()
+entryPoint = go =<< cmdArgs defaultArguments
+  where
+    go (Arguments Push r b) = putStrLn $ "push " ++ r ++ "/" ++ b
+    go (Arguments Pull r b) = putStrLn $ "pull " ++ r ++ "/" ++ b
+    go (Arguments Status _ _)= putStrLn "status"
